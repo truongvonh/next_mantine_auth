a08422dad07719c78dbcd463ba3c34d4db0c391c
VN-OTS-11FQ05N <vonhattruong@Vos-MacBook-Pro.local>
2022-04-24 21:22:40 +0700

    customize document file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,22 +1,11 @@
 import Document, { DocumentContext } from 'next/document';
-import { ServerStyles, createStylesServer } from '@mantine/next';
 
-const stylesServer = createStylesServer();
-
-export default class MyDocument extends Document {
+class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
 
-    // Add your app specific logic here
-
-    return {
-      ...initialProps,
-      styles: (
-        <>
-          {initialProps.styles}
-          <ServerStyles html={initialProps.html} server={stylesServer} />
-        </>
-      ),
-    };
+    return initialProps;
   }
 }
+
+export default MyDocument;
